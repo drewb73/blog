43f424baf62fa102bf5cfc34c2e5817a41885c15
drewb73 <bartondrew5@gmail.com>
2022-05-24 08:07:07 -0700

    post model and controller

---
+++ backend/route/posts/postRoute.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { createPostCtrl } = require('../../controllers/posts/postCtrl')
+const authMiddleware = require('../../middleware/auth/authMiddleware')
+
+
+const postRoute = express.Router()
+
+
+
+postRoute.post('/', authMiddleware, createPostCtrl )
+
+
+
+
+module.exports = postRoute
