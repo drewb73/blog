ff9ed9e06dd4307749bdd2bf14b2f454917def2b
drewb73 <bartondrew5@gmail.com>
2022-05-25 07:45:45 -0700

    comment controller update

--- backend/route/comments/commentRoute.js
+++ backend/route/comments/commentRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createCommentCtrl } = require("../../controllers/comments/commentCtrl");
+const { createCommentCtrl, fetchAllCommentsCtrl } = require("../../controllers/comments/commentCtrl");
 const authMiddleware = require("../../middleware/auth/authMiddleware");
 
 const CommentRoutes = express.Router();
 
 CommentRoutes.post("/", authMiddleware, createCommentCtrl);
+CommentRoutes.get("/", authMiddleware, fetchAllCommentsCtrl);
 
 module.exports = CommentRoutes;
