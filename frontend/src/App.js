b982fe2034638ba30c251fd9cc9675b80f06ce56
drewb73 <bartondrew5@gmail.com>
2022-05-27 08:52:06 -0700

    login page update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Homepage from './components/homepage/Homepage';
 import Register from './components/Users/Register';
+import Login from './components/Users/Login';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Homepage />} />
         <Route exact path='/register' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
